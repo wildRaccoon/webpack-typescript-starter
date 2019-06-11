1bdee25ad821066ebf0070d1d7747c876b3ee560
wildRaccoon <chukvm@gmail.com>
2019-06-12 00:07:06 +0300

    add store sample

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,60 @@
-class App
-{
-    public Log(){
-        var element = document.getElementById("app");
-        if(element)
-        {
-            element.innerText = "Hello Webpack Starter";
-        }
-    }
-};
+import 'reflect-metadata';
+import { StoreModule, Store, ActionsSubject, StateObservable } from "@ngrx/store";
+import { ReflectiveInjector, Provider } from "@angular/core";
 
-var app = new App();
-app.Log();
+//actions
+import { createAction } from '@ngrx/store';
+
+export const increment = createAction('[Counter Component] Increment');
+export const decrement = createAction('[Counter Component] Decrement');
+export const reset = createAction('[Counter Component] Reset');
+
+//reducer
+import { createReducer, on } from '@ngrx/store';
+//import { increment, decrement, reset } from './counter.actions';
+
+export const initialState = 0;
+
+export const counterReducer = createReducer(initialState,
+  on(increment, state => state + 1),
+  on(decrement, state => state - 1),
+  on(reset, state => 0),
+);
+
+//module
+var storeModule = StoreModule.forRoot({ count: counterReducer });
+var providers$:Provider[] = [];
+
+//providers$.push({ provide:ActionsSubject, useClass:ActionsSubject, deps:[] });
+
+if(storeModule.providers)
+{
+    storeModule.providers.forEach(element => {
+       providers$.push(element); 
+    });
+}
+
+
+var injector = ReflectiveInjector.resolveAndCreate(providers$);
+
+// var injector = Injector.create({
+//     providers:providers$
+// });
+
+var store$ = <Store<{ count: number }>>injector.get(Store);
+
+if(store$)
+{
+    console.log("store$");
+}
+
+store$.subscribe((x => console.log(x.count)));
+
+store$.dispatch(increment());
+store$.dispatch(increment());
+store$.dispatch(increment());
+store$.dispatch(increment());
+store$.dispatch(decrement());
+store$.dispatch(decrement());
+store$.dispatch(decrement());
+store$.dispatch(reset());
